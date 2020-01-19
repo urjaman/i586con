2c243702aac896d7193b53dd6fe564048906dbc5
Urja Rannikko <urjaman@gmail.com>
2020-01-20 01:58:04 +0200

    reconfiguring ...

--- i586con_brext/board/post_build.sh
+++ i586con_brext/board/post_build.sh
@@ -11,4 +11,7 @@ grep -q tty4 inittab || sed -i '33 a tty4::respawn:/sbin/getty -L tty4 0 linux' 
 grep -q tty5 inittab || sed -i '34 a tty5::respawn:/sbin/getty -L tty5 0 linux' inittab
 grep -q tty6 inittab || sed -i '35 a tty6::respawn:/sbin/getty -L tty6 0 linux' inittab
 grep -q tty7 inittab || sed -i '36 a tty7::respawn:/usr/bin/tail -f /var/log/messages' inittab
+cd "$1"
+rm -rf usr/share/mc/help/mc.hlp.*
+rm -rf usr/share/zoneinfo/right
 exit 0
