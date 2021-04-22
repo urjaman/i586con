c05ddc70d575e89f795deb39dd6fd4e680e7b4fc
Urja Rannikko <urjaman@gmail.com>
2021-04-22 04:21:19 +0300

    start of updates for buildroot 2021.02.1 / linux-5.10

--- fextract.sh
+++ fextract.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 set -e
 set -x
-BR_V=2020.02.3
+BR_V=2021.02.1
 BR_N=buildroot-$BR_V.tar.bz2
 wget https://buildroot.org/downloads/$BR_N
 tar xf $BR_N
-cp -n 0003-genisoimage-outfile-extern.patch buildroot-$BR_V/package/cdrkit/
