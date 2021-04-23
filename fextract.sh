5ab12249305577d0ea36b0474008f0926b554fc1
Urja Rannikko <urjaman@gmail.com>
2021-04-23 14:38:49 +0300

    patch buildroot to allow links framebuffer graphics

--- fextract.sh
+++ fextract.sh
@@ -3,5 +3,6 @@ set -e
 set -x
 BR_V=2021.02.1
 BR_N=buildroot-$BR_V.tar.bz2
-wget https://buildroot.org/downloads/$BR_N
+[ -e $BR_N ] || wget https://buildroot.org/downloads/$BR_N
 tar xf $BR_N
+(cd buildroot-$BR_V; patch -Np1 < ../0001-package-links-graphics-mode-does-not-depend-on-Direc.patch)
