459a1314742e31e382b47193258cd20b38b37eb4
Urja Rannikko <urjaman@gmail.com>
2021-04-25 01:20:36 +0300

    turn off squashfs xattrs (just in case) and exports (smaller)

--- i586con_brext/external.mk
+++ i586con_brext/external.mk
@@ -5,3 +5,5 @@ target-post-image: host-cdrkit
 # bump the squashfs block size to improve compression ... but not too much,
 # the target computers are old...
 ROOTFS_SQUASHFS_ARGS += -b 256K
+# Also, turn off some things we dont want
+ROOTFS_SQUASHFS_ARGS += -no-exports -no-xattrs
