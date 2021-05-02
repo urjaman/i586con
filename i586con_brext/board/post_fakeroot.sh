74479cfb68b89303c80eeffbd31918e2ea3d9c0f
Urja Rannikko <urjaman@gmail.com>
2021-05-02 03:03:24 +0300

    Homely stuff

--- i586con_brext/board/post_fakeroot.sh
+++ i586con_brext/board/post_fakeroot.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 cd "$1"
+mv root home/
+ln -s home/root root
+sed -i 's#/root#/home/root#g' etc/passwd
 TYPE="$(basename $(dirname $1))"
-[ "$TYPE" = "tar" ] || $HOST_DIR/bin/mksquashfs usr etc ro.sqfs -comp zstd -b 256K -no-exports -no-xattrs
-rm -rf usr etc
-mkdir usr etc
+[ "$TYPE" = "tar" ] || $HOST_DIR/bin/mksquashfs usr etc home ro.sqfs -comp zstd -b 256K -no-exports -no-xattrs
+rm -rf usr etc home
+mkdir usr etc home
 if [ "$TYPE" = "cpio" ]; then
 	echo ro.sqfs | cpio  --quiet -o -H newc > $BUILD_DIR/../images/ro.cpio
 	rm -f ro.sqfs
