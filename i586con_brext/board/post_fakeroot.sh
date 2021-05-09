34bc1467476d536cfae0614c4bef4075487a48c1
Urja Rannikko <urjaman@gmail.com>
2021-05-09 19:06:58 +0300

    prepare for hush as default root shell
    this is for safer operations under low RAM conditions;
    better chance of managing to enable swap or whatever if root
    login gives you something from busybox (which is already in RAM)
    than starting a whole new big bash binary

--- i586con_brext/board/post_fakeroot.sh
+++ i586con_brext/board/post_fakeroot.sh
@@ -3,6 +3,7 @@ cd "$1"
 mv root home/
 ln -s home/root root
 sed -i 's#/root#/home/root#g' etc/passwd
+sed -i '1 s/bash/hush/' etc/passwd
 TYPE="$(basename $(dirname $1))"
 [ "$TYPE" = "tar" ] || $HOST_DIR/bin/mksquashfs usr etc home ro.sqfs -comp zstd -b 256K -no-exports -no-xattrs
 rm -rf usr etc home
--- i586con_brext/board/rootfs-overlay/etc/profile.d/01bashps1.sh
+++
@@ -1,6 +0,0 @@
-if [ -z "${BASH_VERSION-}" ] ; then
-	# Shell is not bash, get out.
-	return
-fi
-
-. /etc/bash/bashrc
