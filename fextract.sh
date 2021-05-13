4017ba2217fb48f277c4a26e67f77d6845a6ff62
Urja Rannikko <urjaman@gmail.com>
2021-05-13 18:28:33 +0300

    Turn off SSE2 from openssl; cleanup fextract, seperate out dl
    SSE2 is not a thing for our target HW, thus byebye.
    
    keeping dl out of buildroot proper makes for leaner buildroot
    switches/patch re-working :)

--- fextract.sh
+++ fextract.sh
@@ -5,7 +5,10 @@ BR_V=2021.02.2
 BR_N=buildroot-$BR_V.tar.bz2
 [ -e $BR_N ] || wget https://buildroot.org/downloads/$BR_N
 tar xf $BR_N
-(cd buildroot-$BR_V; patch -Np1 < ../patches4br/0001-package-links-graphics-mode-does-not-depend-on-Direc.patch)
-(cd buildroot-$BR_V; patch -Np1 < ../patches4br/slimmer-libopenssl.patch)
-(cd buildroot-$BR_V; patch -Np1 < ../patches4br/allow-fuse-module.patch)
-(cd buildroot-$BR_V; patch -Np1 < ../patches4br/links-force-no-libevent.patch)
+mkdir -p dl
+cd buildroot-$BR_V
+ln -s ../dl dl
+patch -Np1 < ../patches4br/0001-package-links-graphics-mode-does-not-depend-on-Direc.patch
+patch -Np1 < ../patches4br/slimmer-libopenssl.patch
+patch -Np1 < ../patches4br/allow-fuse-module.patch
+patch -Np1 < ../patches4br/links-force-no-libevent.patch
