05848fe4f65ca9eace933e676be59c399396e83f
Urja Rannikko <urjaman@gmail.com>
2021-05-13 22:40:29 +0300

    make the qemu script forward ssh to guest
    this way i could actually test the xterm title changing with qemu

--- qemu-install-test.sh
+++ qemu-install-test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 FN=inst-test-$$.qcw2
 qemu-img create -f qcow2 $FN 1G
-qemu-system-i386 -m 60 -hda $FN -cdrom bld/images/i586con.iso -soundhw es1370
+qemu-system-i386 -m 60 -hda $FN -cdrom bld/images/i586con.iso -device ES1370 -device e1000,netdev=net0 -netdev user,id=net0,hostfwd=tcp::1586-:22
