608c6a01303e3576d4a9b0b2a218d34964cffeb6
Urja Rannikko <urjaman@gmail.com>
2021-05-13 13:24:02 +0300

    Move all the buildroot patches to one directory; cleaner this way

--- fextract.sh
+++ fextract.sh
@@ -5,7 +5,7 @@ BR_V=2021.02.2
 BR_N=buildroot-$BR_V.tar.bz2
 [ -e $BR_N ] || wget https://buildroot.org/downloads/$BR_N
 tar xf $BR_N
-(cd buildroot-$BR_V; patch -Np1 < ../0001-package-links-graphics-mode-does-not-depend-on-Direc.patch)
-(cd buildroot-$BR_V; patch -Np1 < ../slimmer-libopenssl.patch)
-(cd buildroot-$BR_V; patch -Np1 < ../allow-fuse-module.patch)
-(cd buildroot-$BR_V; patch -Np1 < ../links-force-no-libevent.patch)
+(cd buildroot-$BR_V; patch -Np1 < ../patches4br/0001-package-links-graphics-mode-does-not-depend-on-Direc.patch)
+(cd buildroot-$BR_V; patch -Np1 < ../patches4br/slimmer-libopenssl.patch)
+(cd buildroot-$BR_V; patch -Np1 < ../patches4br/allow-fuse-module.patch)
+(cd buildroot-$BR_V; patch -Np1 < ../patches4br/links-force-no-libevent.patch)
