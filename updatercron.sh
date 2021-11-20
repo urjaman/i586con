29d61e518527c1fc486921f52494a70a1542e5cb
Urja Rannikko <urjaman@gmail.com>
2021-11-20 06:50:42 +0200

    HMS > pid

--- updatercron.sh
+++ updatercron.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/lib/jvm/default/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl"
 mkdir -p logs
-screen -h 1000 -L -Logfile "logs/$(date +%y%m%d)-$$.log" -dmS i586con-autoupdater -t i586con-bb flock -n updatercron.lock ./autoupdater.py --email
+screen -h 1000 -L -Logfile "logs/$(date +%y%m%d-%H%M%S).log" -dmS i586con-autoupdater -t i586con-bb flock -n updatercron.lock ./autoupdater.py --email
