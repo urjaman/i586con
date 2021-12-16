8ae3435aa1b4b7506498372de43fcacea348a7bd
Urja Rannikko <urjaman@gmail.com>
2021-12-16 08:19:20 +0200

    New signed message format, includes SHA256 (y)
    (also syslinux patch was included in 2021.2.8)

--- fextract.sh
+++ fextract.sh
@@ -7,11 +7,11 @@ BR_N=buildroot-$BR_V.tar.bz2
 [ -e $BR_N.sign ] || wget https://buildroot.org/downloads/$BR_N.sign
 # You'll need to have manually fetched and trusted their gpg key to use --verify
 if [ "$1" == "--verify" ]; then
-	# The signature they provide is an awkward signed message with SHA1 and MD5
-	# Both are kinda weak but checking both is the best i can do...
+	# The signature they provide is an awkward signed message with SHA1 and SHA256
+	# We're checking both to make sure
 	gpg --output signed_sums --verify $BR_N.sign
 	grep 'SHA1:' signed_sums | cut -f 2- -d ' ' | sha1sum -c
-	grep 'MD5:' signed_sums | cut -f 2- -d ' ' | md5sum -c
+	grep 'SHA256:' signed_sums | cut -f 2- -d ' ' | sha256sum -c
 	rm -f signed_sums
 fi
 tar xf $BR_N
@@ -23,5 +23,4 @@ patch -Np1 < ../patches4br/slimmer-libopenssl.patch
 patch -Np1 < ../patches4br/allow-fuse-module.patch
 patch -Np1 < ../patches4br/links-force-no-libevent.patch
 patch -Np1 < ../patches4br/grub2-hostfixes.patch
-patch -Np1 < ../patches4br/syslinux-as-objcopy.patch
 touch ../.fextract-ok-$BR_V
