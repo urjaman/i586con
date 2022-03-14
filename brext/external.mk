dc82b10fbe11111b38797622b7f0a1e41e042532
Urja Rannikko <urjaman@gmail.com>
2022-03-15 00:08:56 +0200

    per-package target dirs compatible forced additions to PACKAGES
    per package does not (yet) work, but maybe some day

--- brext/external.mk
+++ brext/external.mk
@@ -1,14 +1,5 @@
 
-# our post_image script uses genisoimage, so ask buildroot to install it for us
-target-post-image: host-cdrkit
-
-# bump the squashfs block size to improve compression ... but not too much,
-# the target computers are old...
-ROOTFS_SQUASHFS_ARGS += -b 256K
-# Also, turn off some things we dont want
-ROOTFS_SQUASHFS_ARGS += -no-exports -no-xattrs
-
-# Our post_fakeroot script requires mksquashfs, have it installed plz
-rootfs-common: host-squashfs
+# These are required by our scripts
+PACKAGES += host-cdrkit host-squashfs
 
 include $(sort $(wildcard $(BR2_EXTERNAL_I586CON_PATH)/package/*/*.mk))
