16c4af9a31c89017e88e09ec7c0a90095cb685bf
Urja Rannikko <urjaman@gmail.com>
2024-03-30 12:06:26 +0200

    linux-vt-setcolors: refresh

--- brext/package/linux-vt-setcolors/linux-vt-setcolors.mk
+++ brext/package/linux-vt-setcolors/linux-vt-setcolors.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-LINUX_VT_SETCOLORS_VERSION = 99ec18bee1d2a2c062f59040bb25e2288905b8bc
-LINUX_VT_SETCOLORS_SITE = $(call github,EvanPurkhiser,linux-vt-setcolors,$(LINUX_VT_SETCOLORS_VERSION))
+LINUX_VT_SETCOLORS_VERSION = 1dccf7c77f43331b671b6a401c06cf5d919812df
+LINUX_VT_SETCOLORS_SITE = $(call github,evanpurkhiser,linux-vt-setcolors,$(LINUX_VT_SETCOLORS_VERSION))
 LINUX_VT_SETCOLORS_LICENSE = MIT
 LINUX_VT_SETCOLORS_LICENSE_FILES = LICENSE
 
